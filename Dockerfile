3caa196d8ffc1474b896992296e2440bea2d7f50
William <williamlangroudi@gmail.com>
2020-07-20 13:46:53 +0200

    - Add logging for db connection - Update docker to use watch script (hot reloading not working yet)

--- Dockerfile
+++ Dockerfile
@@ -9,4 +9,4 @@ RUN npm install
 COPY . .
 EXPOSE 3001
 
-CMD ["npm", "run", "start"]
+CMD ["npm", "run", "start:dev"]
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import mongoose from 'mongoose';
 import { AppModule } from './app.module';
 import { config } from './config';
 import { ENVIRONMENT } from './constants';
-const { server } = config[ENVIRONMENT];
+const { database, server } = config[ENVIRONMENT];
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(server.port);
+
   console.log(
     '[ThreeSprints IoT API Running on]:',
     `http://${server.host}:${server.port} - [Environment: ${ENVIRONMENT}]`,
   );
+
+  console.log('[Connected to DB]:', database.url);
 }
 bootstrap();
