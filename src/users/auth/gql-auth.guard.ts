b0b41e8660ca6da2754cc505e783e0e05f9c935b
William <williamlangroudi@gmail.com>
2020-07-22 14:10:16 +0200

    - Add in-memory cache - Add getting and settings jwks from cache (refreshed hourly) for authentication

--- src/users/auth/gql-auth.guard.ts
+++ src/users/auth/gql-auth.guard.ts
@@ -5,22 +5,29 @@ import {
   HttpException,
   HttpStatus,
   Inject,
+  OnModuleInit,
 } from '@nestjs/common';
 import { GqlExecutionContext } from '@nestjs/graphql';
 import Axios from 'axios';
 import * as jwt from 'jsonwebtoken';
 import jwkToPem from 'jwk-to-pem';
-
+import { Cache } from 'cache-manager';
 import { AuthConfig } from './auth.config';
 
 @Injectable()
-export class GqlAuthGuard implements CanActivate {
+export class GqlAuthGuard implements CanActivate, OnModuleInit {
   private jwkUrl = `https://cognito-idp.${this.authConfig.region}.amazonaws.com/${this.authConfig.userPoolId}/.well-known/jwks.json`;
 
   constructor(
     @Inject('AuthConfig')
     private readonly authConfig: AuthConfig,
+    @Inject('CACHE_MANAGER') private cacheManager: Cache,
   ) {}
+
+  async onModuleInit(): Promise<void> {
+    const jwks = (await Axios.get(this.jwkUrl)).data.keys as any;
+    this.cacheManager.set('jwks', jwks, { ttl: 3600 });
+  }
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const ctx = GqlExecutionContext.create(context).getContext();
@@ -36,6 +43,24 @@ export class GqlAuthGuard implements CanActivate {
     } catch (err) {
       return false;
     }
+  }
+
+  private async _getJwks() {
+    const cachedJwks = this.cacheManager.get('jwks');
+    if (cachedJwks) {
+      return cachedJwks;
+    } else {
+      const jwks = (await Axios.get(this.jwkUrl)).data.keys as any;
+      this.cacheManager.set('jwks', jwks, { ttl: 3600 });
+      return jwks;
+    }
+  }
+
+  private _getTokenFromHeader(authHeader: string): string {
+    if (authHeader.split(' ')[0] !== 'Bearer') {
+      throw new HttpException('Invalid token', HttpStatus.UNAUTHORIZED);
+    }
+    return authHeader.split(' ')[1];
   }
 
   private _verifyToken(
@@ -62,12 +87,9 @@ export class GqlAuthGuard implements CanActivate {
   }
 
   async validateToken(authHeader: string) {
-    if (authHeader.split(' ')[0] !== 'Bearer') {
-      throw new HttpException('Invalid token', HttpStatus.UNAUTHORIZED);
-    }
-    const token = authHeader.split(' ')[1];
     try {
-      const jwks = (await Axios.get(this.jwkUrl)).data.keys as any;
+      const token = this._getTokenFromHeader(authHeader);
+      const jwks = this._getJwks();
       const pem = jwkToPem(jwks[1]);
       return await this._verifyToken(token, pem, { algorithms: ['RS256'] });
     } catch (err) {
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,14 +1,17 @@
-import { Module } from '@nestjs/common';
+import { Module, CacheModule } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { UsersService } from './users.service';
 import { UsersResolver } from './users.resolver';
 import { UsersRepository } from './users.repository';
-import { MongooseModule } from '@nestjs/mongoose';
 import { UserSchema } from './users.schema';
 import { AuthService } from './auth/auth.service';
 import { AuthConfig } from './auth/auth.config';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: 'User', schema: UserSchema }])],
+  imports: [
+    MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
+    CacheModule.register(),
+  ],
   providers: [
     UsersService,
     UsersRepository,
