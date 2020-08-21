e48c1b9525fde848058bf340c56ad2221fea067c
william <williamlangroudi@gmail.com>
2020-08-21 20:33:41 +0200

    - Remove logs and update environment variables

--- src/main.ts
+++ src/main.ts
@@ -6,14 +6,12 @@ import { BadRequestTransformer } from './users/auth/error-transformer.filter';
 import { AppConfigService } from './config/configuration.service';
 
 async function bootstrap() {
-  console.log({ENVIRONMENT_VARIABLES: process.env});
-  const appUrl = process.env.APP_URL || 'http://localhost:3001';
   // Add node-fetch to globals (used in interfacing with aws)
   global['fetch'] = require('node-fetch');
   const app = await NestFactory.create(AppModule, {
     cors: {
       credentials: true,
-      origin: process.env.IOT_UI_URL || 'http://localhost:3000',
+      origin: process.env.IOT_UI_URL,
     },
   });
 
@@ -26,7 +24,7 @@ async function bootstrap() {
 
   console.log(
     '[ThreeSprints IoT API Running on]:',
-    `${appUrl} - [Environment: ${appConfig.appEnv}]`,
+    `${appConfig.appUrl} - [Environment: ${appConfig.appEnv}]`,
   );
   console.log(
     '[Connected to DB]:',
