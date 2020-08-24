3d721056cce5db20a0c53a272d479dfcf5dbfa67
William <williamlangroudi@gmail.com>
2020-08-24 09:37:02 +0200

    Re order imports in main (for testing github action)

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
+import { AppModule } from './app.module';
 import { BadRequestTransformer } from './users/auth/error-transformer.filter';
 import { AppConfigService } from './config/configuration.service';
 
