7081f43c218231475d7c0a992f489ba12793429a
William <williamlangroudi@gmail.com>
2020-09-03 12:39:42 +0200

    - Add test for expired claims - Refactor tests (remove repeated code)

--- src/users/auth/auth.guard.spec.ts
+++ src/users/auth/auth.guard.spec.ts
@@ -1,4 +1,4 @@
-import { CACHE_MANAGER, HttpException, } from '@nestjs/common';
+import { CACHE_MANAGER, HttpException, HttpStatus } from '@nestjs/common';
 import { AuthGuard } from "./auth.guard";
 import { Test } from "@nestjs/testing";
 import { AuthConfig } from "./auth.config";
@@ -36,7 +36,7 @@ const invalidJWTAuthHeaderMock = authHeaderMock.replace('Bearer', 'Chewbacca');
 const jwtTokenMock = "eyJraWQiOiJJakY0bnRnNjh0bGxDdlJMT1Q4Q21rNG91SFFOcTBtQVRIa2RvNXBROFI0PSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiJkMGJjODI0Yi1iZGE5LTQ4YWQtOWNjOS01Zjc3N2JiODQ1OTIiLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiaXNzIjoiaHR0cHM6XC9cL2NvZ25pdG8taWRwLmV1LXdlc3QtMi5hbWF6b25hd3MuY29tXC9ldS13ZXN0LTJfUmlYcXRpR0YxIiwiY29nbml0bzp1c2VybmFtZSI6IjEyMyIsImdpdmVuX25hbWUiOiJXaWxsaWFtIiwiYXVkIjoiNXJqMmkzZjRhbzM0aGNja29iYmxyNjFzaWsiLCJldmVudF9pZCI6ImQ1YWM3ZDRiLWZjZWQtNDZiZC1iMzc2LTc0OTUzMDczYzY2YSIsInRva2VuX3VzZSI6ImlkIiwiYXV0aF90aW1lIjoxNTk4OTUwMDcwLCJleHAiOjE1OTg5NTM2NzAsImlhdCI6MTU5ODk1MDA3MCwiZmFtaWx5X25hbWUiOiJMYW5nIiwiZW1haWwiOiJ3aWxsaWFtKzEyM0B0aHJlZXNwcmludHMuY29tIn0.j8gEOlirnY_gBiL7ne7d3gKLQ5YXBk34jsSj4F1hrPuvkqRTblQsAsMIV2OrKCJydzxxm8zT_QQz6NjRC6TjXxaFzYbWdIQHJJBwE1GPjNiZIqK1VGu0cxGkPNETBMPxMXYM_euGCXjJQIQ3-zEeBq3Xkd-MYEKIzMWRng3LiiSIpkz3SeXABeFj0vv3Hh5ezMyBMSrBIcjWP_eE_lhDtvPwep54UXM8yYdAH_B4SRy4ll3KgGPRkBz8AT1qowKGvXRJjkfgII0HtxzqSsXRvRwRxxR-aLMqG42W-MvklT18wSSG-1nYZBHTgb4a3C_1u6V_dUB6mnMt1e-JE3dUkw"
 const invalidJWTTokenMock = 'Bearer v86at79y82.24570aipRl';
 const decodedJwtHeaderMock = {
-    "kid": `IjF4ntg68tllCvRLOT8Cmk4ouHQNq0mATHkdo5pQ8R4=`,
+    "kid": "IjF4ntg68tllCvRLOT8Cmk4ouHQNq0mATHkdo5pQ8R4=",
     "alg": "RS256"
 };
 
@@ -50,10 +50,19 @@ const claimMock = {
     username: "123",
     client_id: "testId"
 };
+const expiredClaimMock = {
+    token_use: "id",
+    auth_time: tenMinutesAgo,
+    iss: `https://cognito-idp.${process.env.COGNITO_REGION}.amazonaws.com/${process.env.COGNITO_USER_POOL_ID}`,
+    exp: tenMinutesAgo,
+    username: "123",
+    client_id: "testId"
+};
 
 
 describe('AuthGuard', () => {
     let authGuard: AuthGuard;
+    let authGuardProto: any;
     beforeEach(async () => {
         const module = await Test.createTestingModule({
             providers: [
@@ -68,6 +77,7 @@ describe('AuthGuard', () => {
                 }],
         }).compile();
         authGuard = module.get(AuthGuard);
+        authGuardProto = Object.getPrototypeOf(authGuard);
     });
 
     it('should be defined', () => {
@@ -77,7 +87,6 @@ describe('AuthGuard', () => {
     describe('validateToken()', () => {
         it('returns validation result', async () => {
             expect.assertions(1);
-            const authGuardProto = Object.getPrototypeOf(authGuard);
             jest.spyOn(authGuardProto, '_getEncodedAuthTokenFromHeader').mockReturnValueOnce(jwtTokenMock);
             jest.spyOn(authGuardProto, '_getDecodedJwtHeader').mockReturnValueOnce(decodedJwtHeaderMock);
             jest.spyOn(authGuardProto, '_getPublicKeyDictionary').mockResolvedValueOnce(publicKeyDictionaryMock);
@@ -91,7 +100,6 @@ describe('AuthGuard', () => {
         });
         it('throws an HttpException if a JWT auth header without "Bearer" is given', async () => {
             expect.assertions(2);
-            const authGuardProto = Object.getPrototypeOf(authGuard);
             const _getEncodedAuthTokenFromHeaderSpy = jest.spyOn(authGuardProto, '_getEncodedAuthTokenFromHeader');
             try {
                 await authGuard.validateToken(invalidJWTAuthHeaderMock);
@@ -102,13 +110,23 @@ describe('AuthGuard', () => {
         });
         it('throws an HttpException if an invalid JWT token header is given with auth header "Bearer"', async () => {
             expect.assertions(1);
-            const authGuardProto = Object.getPrototypeOf(authGuard);
             jest.spyOn(authGuardProto, '_getDecodedJwtHeader').mockReturnValueOnce('v86at79y82');
             jest.spyOn(authGuardProto, '_getPublicKeyDictionary').mockReturnValueOnce(publicKeyDictionaryMock);
             try {
                 await authGuard.validateToken(invalidJWTTokenMock);
             } catch (error) {
                 expect(error.message).toMatch('Claim made for unknown kid');
+            }
+        });
+        it('throws an HttpException if an expired token is given', async () => {
+            jest.spyOn(authGuardProto, '_getEncodedAuthTokenFromHeader').mockReturnValueOnce(jwtTokenMock);
+            jest.spyOn(authGuardProto, '_getDecodedJwtHeader').mockReturnValueOnce(decodedJwtHeaderMock);
+            jest.spyOn(authGuardProto, '_getPublicKeyDictionary').mockResolvedValueOnce(publicKeyDictionaryMock);
+            jest.spyOn(authGuardProto, '_verifyToken').mockResolvedValueOnce(expiredClaimMock);
+            try {
+                await authGuard.validateToken(authHeaderMock);
+            } catch (error) {
+                expect(error.message).toMatch('Expired claim');
             }
 
         });
@@ -117,13 +135,11 @@ describe('AuthGuard', () => {
     describe('_getEncodedAuthTokenFromHeader()', () => {
         it('returns a jwt token from an authorization header', () => {
             expect.assertions(1);
-            const authGuardProto = Object.getPrototypeOf(authGuard);
             const result = authGuardProto._getEncodedAuthTokenFromHeader(authHeaderMock);
             expect(result).toEqual(authHeaderMock.split(' ')[1]);
         });
         it('throws an HttpException if a jwt auth header without Bearer is given', () => {
             expect.assertions(1);
-            const authGuardProto = Object.getPrototypeOf(authGuard);
             expect(() => authGuardProto._getEncodedAuthTokenFromHeader(invalidJWTAuthHeaderMock)).toThrow(HttpException);
         });
         it('throws an HttpException if an invalid jwt auth header is given', () => {
@@ -136,20 +152,16 @@ describe('AuthGuard', () => {
 
     describe('_getDecodedJwtHeader()', () => {
         it('returns the decoded header section of an encoded JWT token', () => {
-            expect.assertions(2);
-            const authGuardProto = Object.getPrototypeOf(authGuard);
+            expect.assertions(1);
             const result = authGuardProto._getDecodedJwtHeader(jwtTokenMock);
-            expect(result).toHaveProperty('kid');
-            expect(result).toHaveProperty('alg');
+            expect(result).toEqual(decodedJwtHeaderMock);
         });
         it('throws an HttpException if given token doesn\'t have period separated sections\n', () => {
             expect.assertions(1);
-            const authGuardProto = Object.getPrototypeOf(authGuard);
             expect(() => authGuardProto._getEncodedAuthTokenFromHeader(jwtTokenMock)).toThrow(HttpException);
         });
         it('throws an HttpException if an invalid jwt auth header is given', () => {
             expect.assertions(1);
-            const authGuardProto = Object.getPrototypeOf(authGuard);
             expect(() => authGuardProto._getEncodedAuthTokenFromHeader('ya7u8vijwjankll[vwa')).toThrow(HttpException);
         });
 
